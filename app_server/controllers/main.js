985147865edcf2eaa661f41fa00e1082a01229f3
Asia Peters <asia.peters@snhu.edu>
2025-04-30 20:06:30 -0700

    Set foundation for featured listings with Handlebars - Created listings.json in the data folder for all listings - Updated main.js controller to read listings data using fs.readFileSync() - Replaced static HTML featured listings with Handlebars {{#each}} loop in index.hbs - Rendered featured listings dynamically with listing names and descriptions - Renamed image files to align with new page structure

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,8 +1,12 @@
+var fs = require('fs');
+var featuredListings = JSON.parse(fs.readFileSync('./data/listings.json','utf8'));
+
 /* GET home page view */
 
 const index = (req, res) => {
     res.render("index", {
         title: "RentalConnect - Affordable College Rentals",
+        featuredListings
     });
 };
 
