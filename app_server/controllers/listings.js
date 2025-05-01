555ab1e4c9b75e197885e476e0ffed170a40c95d
Asia Peters <asia.peters@snhu.edu>
2025-04-30 20:19:19 -0700

    Set foundation for listings with Handlebars - Updated listings.js controller to read listings data using fs.readFileSync() - Replaced static HTML listings with Handlebars {{#each listings}} loop in listings.hbs - Rendered listings dynamically list names and descriptions

--- app_server/controllers/listings.js
+++ app_server/controllers/listings.js
@@ -1,8 +1,12 @@
+var fs = require('fs');
+var rentalListings = JSON.parse(fs.readFileSync('./data/listings.json','utf8'));
+
 /* GET listings view */
 
 const listings = (req, res) => {
     res.render("listings", {
         title: "RentalConnect - Available Listings",
+        rentalListings
     });
 };
 
